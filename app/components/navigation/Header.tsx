ece2280b4ba418a405047578c5e042c2c8d2a9e8
Ogechi Ikwunagu <54102855+ogechinagu@users.noreply.github.com>
2024-04-15 14:24:36 +0000

    header component done

--- app/components/navigation/Header.tsx
+++ app/components/navigation/Header.tsx
@@ -1,12 +1,31 @@
 import Link from "next/link"
+import { FaGithub,FaLinkedinIn,FaThreads,FaXTwitter } from "react-icons/fa6";
 
 const Header = () => {
+
+    const SocialMedia = [
+        {route: 'https://twitter.com/ogechinagu', color: 'bg-[#a2d2ff]', name: 'Twitter', icon: <FaXTwitter />},
+        {route: 'https://www.linkedin.com/in/ogechinagu/', color: 'bg-[#ffafcc]', name: 'LinkedIn', icon: <FaLinkedinIn />},
+        {route: 'https://www.threads.net/@ogechinagu', color: 'bg-[#bde0fe]', name: 'Threads', icon: <FaThreads  />},
+        {route: 'https://github.com/ogechinagu', color: 'bg-[#ffc8dd]', name: 'GitHub', icon: <FaGithub  />},
+    ]
     return (
-        <header className="flex">
+        <header className=" flex justify-between items-center px-8 py-4 absolute top-0 w-full">
             <Link href={'/'}>
-                <p className="text-lg font-black tracking-tight text-sky-600">OGECHINAGU</p>
+                <p className="text-lg font-black tracking-tight text-[#a2d2ff]">OGECHINAGU</p>
             </Link>
-            <div>
+            <div className="flex gap-2">
+                {
+                    SocialMedia.map((link) => (
+                        <Link 
+                        href={'/'} 
+                        key={link.name}
+                        className={`${link.color} p-2 text-lg rounded-full rounded-bl-none hover:bg-gray-900 hover:text-white hover:rounded-bl-full hover:rounded-tl-none transition-all duration-500`}
+                        >
+                            {link.icon}
+                        </Link>
+                    ))
+                }
                 
             </div>
 
