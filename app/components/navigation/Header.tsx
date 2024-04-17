224947bd99e068bef7603b7ef71f727dd760a31e
Ogechi Ikwunagu <54102855+ogechinagu@users.noreply.github.com>
2024-04-17 14:29:05 +0000

    completed sidebar, fixed header style

--- app/components/navigation/Header.tsx
+++ app/components/navigation/Header.tsx
@@ -1,13 +1,16 @@
+'use client'
+
+import { motion } from "framer-motion";
 import Link from "next/link"
-import { FaGithub,FaLinkedinIn,FaThreads,FaXTwitter } from "react-icons/fa6";
+import { FaGithub, FaLinkedinIn, FaThreads, FaXTwitter } from "react-icons/fa6";
 
 const Header = () => {
 
     const SocialMedia = [
-        {route: 'https://twitter.com/ogechinagu', color: 'bg-[#a2d2ff]', name: 'Twitter', icon: <FaXTwitter />},
-        {route: 'https://www.linkedin.com/in/ogechinagu/', color: 'bg-[#ffafcc]', name: 'LinkedIn', icon: <FaLinkedinIn />},
-        {route: 'https://www.threads.net/@ogechinagu', color: 'bg-[#bde0fe]', name: 'Threads', icon: <FaThreads  />},
-        {route: 'https://github.com/ogechinagu', color: 'bg-[#ffc8dd]', name: 'GitHub', icon: <FaGithub  />},
+        { route: 'https://twitter.com/ogechinagu', color: 'bg-[#a2d2ff]', name: 'Twitter', icon: <FaXTwitter /> },
+        { route: 'https://www.linkedin.com/in/ogechinagu/', color: 'bg-[#ffafcc]', name: 'LinkedIn', icon: <FaLinkedinIn /> },
+        { route: 'https://www.threads.net/@ogechinagu', color: 'bg-[#bde0fe]', name: 'Threads', icon: <FaThreads /> },
+        { route: 'https://github.com/ogechinagu', color: 'bg-[#ffc8dd]', name: 'GitHub', icon: <FaGithub /> },
     ]
     return (
         <header className=" flex justify-between items-center px-8 py-4 absolute top-0 w-full">
@@ -17,18 +20,14 @@ const Header = () => {
             <div className="flex gap-2">
                 {
                     SocialMedia.map((link) => (
-                        <Link 
-                        href={'/'} 
-                        key={link.name}
-                        className={`${link.color} p-2 text-lg rounded-full rounded-bl-none hover:bg-gray-900 hover:text-white hover:rounded-bl-full hover:rounded-tl-none transition-all duration-500`}
-                        >
-                            {link.icon}
+                        <Link href={link.route} key={link.name}>
+                            <motion.div whileHover={{ scale: 1.2 }} className={`${link.color} p-2 text-lg rounded-full hover:bg-gray-900 hover:text-white hover:rounded-bl-full transition-all duration-500`} >
+                                {link.icon}
+                            </motion.div>
                         </Link>
                     ))
                 }
-                
             </div>
-
         </header>
     )
 }
--- app/components/navigation/Sidenav.tsx
+++ app/components/navigation/Sidenav.tsx
@@ -1,8 +1,39 @@
-const Sidenav = ()  => {
+'use client'
+
+import { motion } from "framer-motion";
+import Link from "next/link"
+import { usePathname } from "next/navigation";
+import { MdHome, MdMail, MdPerson, MdWork } from "react-icons/md";
+
+
+const Sidenav = () => {
+
+    const pathname = usePathname()
+
+    console.log('Pathname: ', pathname)
+
+    const navlinks = [
+        { name: 'Home', icon: <MdHome />, path: '/' },
+        { name: 'About', icon: <MdPerson />, path: '/about' },
+        { name: 'Projects', icon: <MdWork />, path: '/projects' },
+        { name: 'Contact', icon: <MdMail />, path: '/contact' },
+    ]
+
     return (
-        <section>
-            Side Navigation
-        </section>
+        <nav className='fixed max-lg:bottom-2 max-lg:w-full flex items-center justify-center z-50 lg:left-2 lg:h-full'>
+            <div className='flex gap-x-6 lg:gap-x-10 px-4 py-3 lg:py-6 rounded-full bg-stone-300 lg:flex-col lg:gap-y-10'>
+                {navlinks.map((link) => (
+                    <Link key={link.name} href={link.path} className={`${link.path === pathname ? "text-white" : "text-['#141414']"}`}>
+                        <motion.div whileHover={{ scale: 1.2 }} className="bg-[#14141419] relative group rounded-full p-1 lg:p-2">
+                            <motion.span className="h-8 w-8 bold-18 z-50">{link.icon}</motion.span>
+                            <span className='absolute max-lg:bottom-[50px] max-lg:-right-3 lg:left-[50px] lg:top-2 text-white bg-stone-600 text-sm px-2 rounded-md hidden group-hover:flex'>{link.name}
+                            <span className="h-2 w-2 bg-stone-600 absolute max-lg:top-4 max-lg:left-5 translate-x-1/2 lg:top-[6px] lg:left-[-6px] rotate-45 rounded-sm"></span>
+                            </span>
+                        </motion.div>
+                    </Link>
+                ))}
+            </div>
+        </nav>
     )
 }
 
